f8996ffcd8e9a6ccc0135d095b455297e94dbb37
darryl west <dpw@raincitysoftware.com>
2023-01-12 09:54:11 -0800

    startup and stop worker pool

--- src/cache/supervisor.rs
+++ src/cache/supervisor.rs
@@ -1,14 +1,36 @@
-use super::worker::Worker;
+use crate::cache::worker::{Command, Worker};
+use anyhow::Result;
+use log::*;
 
 // add generics to this based on the WorkerTrait
 #[derive(Debug, Default)]
 pub struct Supervisor {
+    pub pool_size: usize,
     pub workers: Vec<Worker>,
 }
 
 impl Supervisor {
-    pub fn new() -> Supervisor {
-        Supervisor { workers: vec![] }
+    /// create and start the worker pool
+    pub async fn new(pool_size: usize) -> Result<Supervisor> {
+        let mut workers = vec![];
+        for _ in 0..pool_size {
+            let worker = Worker::new().await;
+            workers.push(worker);
+        }
+
+        Ok(Supervisor { pool_size, workers })
+    }
+
+    /// shut the workers down
+    pub async fn shutdown(&self) -> Result<()> {
+        for worker in self.workers.iter() {
+            info!("shut worker, id: {} down", worker.id());
+            let tx = worker.request_channel();
+            let r = tx.send(Command::Shutdown).await;
+            info!("ok? {:?}", r);
+        }
+
+        Ok(())
     }
 }
 
@@ -18,8 +40,14 @@ mod tests {
 
     #[test]
     fn new() {
-        let pool_size = 0usize;
-        let supervisor = Supervisor::new();
-        assert_eq!(supervisor.workers.len(), pool_size);
+        async_std::task::block_on(async move {
+            let pool_size: usize = 4;
+            let supervisor = Supervisor::new(pool_size)
+                .await
+                .expect("should create the supervisor");
+            assert_eq!(supervisor.workers.len(), pool_size);
+
+            assert!(supervisor.shutdown().await.is_ok());
+        });
     }
 }
